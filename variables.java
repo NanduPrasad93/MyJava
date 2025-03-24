aad5240fa9e17d8186819e27cefc0d4c4d5bb2b2
nandu <nandhu860696@gmail.com>
2025-03-24 19:21:31 +0530

    variables

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        String name = "Nandhu Prasad";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+
+    }
+}

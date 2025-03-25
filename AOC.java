07247cea8e391d56232c64b9381a2d3246873a0a
nandu <nandhu860696@gmail.com>
2025-03-25 11:14:03 +0530

    area of a circle

---
+++ AOC.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.Math;
+
+public class AOC {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float radius = sc.nextFloat();
+        float area = (float)Math.PI * radius * radius;
+        System.out.println(area);
+    }
+}

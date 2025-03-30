ab33987363311f73075a7881f3712b1e0bd05d4e
nandu <nandhu860696@gmail.com>
2025-03-30 17:33:11 +0530

    pani or not

---
+++ Havefeverornot.java
@@ -0,0 +1,12 @@
+public class Havefeverornot {
+    public static void main(String args[]){
+        double temp = 103.5;
+        if(temp > 100){
+            System.out.println("you got fever buddy");
+        }
+        else{
+            System.out.println("go to school");
+        }
+    }
+    
+}

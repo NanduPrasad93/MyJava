0e2da28594089e18e8eb593c5ef3ec2e3edcf6a4
nandu <nandhu860696@gmail.com>
2025-03-30 17:23:39 +0530

    switch

--- CalculatorUsingSwitch.java
+++ CalculatorUsingSwitch.java
@@ -1,3 +1,21 @@
+import java.util.*;
 public class CalculatorUsingSwitch {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("enter 1 for additon , 2 for subtration ,3 for divition and 4 for multiplication");
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1:
+                System.out.print(a + b);
+                break;
+            case 2:
+                System.out.print(a - b);
+                break;
+            default:
+                System.out.println("get out moron");
+        }
+    }
 }

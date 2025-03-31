5206db932cde5aeb868f87c41d4bb108adb8b50b
nandu <nandhu860696@gmail.com>
2025-03-31 11:57:30 +0530

    commit

---
+++ Dowhile.java
@@ -0,0 +1,9 @@
+public class Dowhile {
+    public static void main(String args[]){
+        int n = 1;
+        do{
+            System.out.println("Hello world");
+            n++;
+        }while(n<=5);
+    }
+}
---
+++ ReverseAnumber.java
@@ -0,0 +1,10 @@
+public class ReverseAnumber {
+    public static void main(String args[]){
+        int num = 10897;
+        while(num > 0){
+            int reminder = num % 10;
+            System.out.print(reminder);
+            num = num/10;
+        }
+    }
+}

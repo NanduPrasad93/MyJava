4a84e2602b3f0fdf5ac631f0e6fef20f2294c01b
nandu <nandhu860696@gmail.com>
2025-03-31 12:31:17 +0530

    commit

---
+++ Break.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Break{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("enter a number");
+            int num = sc.nextInt();
+            if(num%10 == 0)
+                break;
+        }
+        System.out.println("you entered multiple of ten");
+        sc.close();
+    }
+}

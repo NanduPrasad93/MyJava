1a2f364c3cf9b0f64e965f6eea39f467e526178e
nandu <nandhu860696@gmail.com>
2025-04-01 19:33:05 +0530

    commit

--- pattern4.java
+++ pattern4.java
@@ -1,4 +1,14 @@
 public class pattern4 {
-    p
+    public static void main(String args[]){
+        char ch = 'A';
+        int n = 4;
+        for(int line = 1 ; line <= n ; line++){
+            for(int j = 1; j <= line; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
     
 }
